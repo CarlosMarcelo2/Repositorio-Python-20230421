2ce402041d3b515ae056d9086b8f437a9a318188
CarlosMarcelo2 <cmsamamer@gmail.com>
2023-04-21 17:30:05 -0400

    initial commit

---
+++ CLASE2/CLASE3/Clase3.py
@@ -0,0 +1,37 @@
+first_name="Carlos"
+last_neme="Samamé"
+print(first_name + " " + last_neme)
+direccion="El Arrayán"
+numero="165"
+print(direccion + " "+numero)
+multiplicar = "Hola " *3
+print(multiplicar)
+palabra="hola"
+print(palabra)
+
+
+cadena = "esto es un texto de prueba"
+posicion = cadena.find("texto")
+
+empty = []
+print(empty)
+full = [1, 3, 500, [2, "a"], {"name": "Carlos"}]
+print(full)
+
+print(list("Concurso"))
+range_one = range(50)
+print(list(range_one))
+
+numero = [1, 5, 8]
+print(numero)
+numero.append(10)
+print(numero)
+print(numero[2])
+
+lista = [a, b, c, d, e, f]
+for i in lista:
+print(lista)
+
+
+
+      
---
+++ CLASE2/hola.py
@@ -0,0 +1,14 @@
+print ("Hola PRIMERA LINEA")
+edad = 20
+sueldo = 50.00000
+nombre = "Carlos"
+isteacher = True
+
+print(edad,sueldo,nombre,isteacher)
+
+numero_1 = 1
+numero_2 = 100
+resultado = (numero_1 * numero_2)
+print(resultado)
+
+
